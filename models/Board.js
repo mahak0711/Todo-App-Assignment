9944dad145ae25cbb0c3364a2e75017699fa14c4
Mahak <128406559+mahak0711@users.noreply.github.com>
2026-01-29 16:41:18 +0530

    implement board controller(ownership validation)

---
+++ models/Board.js
@@ -0,0 +1,18 @@
+const mongoose=require("mongoose");
+
+const boardSchema=new mongoose.Schema({
+    title:{
+        type:String,
+        requires:true,
+        trim:true
+    },
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        requires:true
+    }
+
+},
+{    timestamps:true}
+);
+module.exports=mongoose.model("Board",boardSchema);
